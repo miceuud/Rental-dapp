63ca7289db76a2827784cef5b3e21186c48df699
miceuud <miceuud@.gmail.com>
2021-12-05 10:20:45 +0100

    rental component added

---
+++ src/components/Rental.vue
@@ -0,0 +1,64 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+  components: {},
+  methods: {},
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+/* h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+
+.main-home {
+  font-weight: bold;
+  font-size: 2rem;
+  text-decoration: none;
+  font-family: "Comforter Brush", cursive;
+}
+#nav {
+  margin-top: 0.5rem;
+}
+.nav-list {
+  position: absolute;
+  top: 0;
+  right: 2rem;
+}
+ul {
+  display: flex;
+  list-style: none;
+}
+li {
+  margin-right: 1rem;
+  font-size: 14px;
+  border: 1px solid black;
+  padding: 7px 8px;
+  border-radius: 5px;
+}
+li a {
+  text-decoration: none;
+  color: black;
+}
+li:nth-of-type(2) {
+  margin-right: 0;
+} */
+</style>
