3055a7588de1312b3abe18e60d1bf69d526c5e40
miceuud <miceuud@.gmail.com>
2021-12-05 10:06:32 +0100

    Dashboard and home views

---
+++ src/views/Dashboard.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="about">
+    <h1>Car rental by You</h1>
+    <div></div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      rentedCar: [],
+    };
+  },
+};
+</script>
+
+<style></style>
